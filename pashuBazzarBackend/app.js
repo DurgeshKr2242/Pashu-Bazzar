5119272b5d716c004d4b8e4f7e470df04c7c5f09
DurgeshKr2242 <code.durgesh@gmail.com>
2021-11-29 21:21:20 +0530

    MAJOR CHANGES

--- pashuBazzarBackend/app.js
+++ pashuBazzarBackend/app.js
@@ -2,24 +2,28 @@ const express = require("express");
 // const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 require("dotenv").config();
+var cors = require("cors");
 
 const postsRoutes = require("./routes/postsRoutes");
 const usersRoutes = require("./routes/usersRoutes");
 const HttpError = require("./models/httpError");
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+//     "access-control-allow-origin"
+//   );
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
 
-  next();
-});
+//   next();
+// });
 
 app.use("/api/posts", postsRoutes);
 app.use("/api/users", usersRoutes);
@@ -42,7 +46,7 @@ mongoose
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@pashubazzar.ezb4x.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
   .then(() => {
-    app.listen(5000);
+    app.listen(process.env.PORT || 5000);
   })
   .catch((e) => {
     console.log(e);
